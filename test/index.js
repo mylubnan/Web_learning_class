2a1d5b572ad6051db82ad7131a214ec7d760fa87
TheNone404 <75447408+TheNone404@users.noreply.github.com>
2023-01-03 13:02:33 +0700

    nav bar done
    nav bar done

--- test/index.js
+++ test/index.js
@@ -1,1 +1,8 @@
-let name = "Lubnan";
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
